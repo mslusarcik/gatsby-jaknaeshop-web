6e90ce445abad20c5409add09205e16becd42130
mslusarcik <miroslav@slusarcik.cz>
2023-03-06 11:06:44 +0100

    extended classes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#fff",
+        "white-v2": "#F0F4EF",
+        primary: "#FEC82E",
+        secondary: "#222",
+        tertiary: "#A51E23",
+        greyV1: "#555",
+        greyV2: "#8B828F",
+      },
+      borderRadius: {
+        egg: "80% 15% 55% 50% / 55% 15% 80% 50%",
+      },
+    },
   },
   plugins: [],
 }
